5a8a5629d65f2a84826d531397513aa4a629916f
Evgeniy Kozhich <wanderbon95@gmail.com>
2020-02-04 11:46:09 +0300

    Update ClearCacheModule.java

--- android/src/main/java/com/wanderbon/clearcahce/ClearCacheModule.java
+++ android/src/main/java/com/wanderbon/clearcahce/ClearCacheModule.java
@@ -20,7 +20,7 @@ public class ClearCacheModule extends ReactContextBaseJavaModule {
 
     @Override
     public String getName() {
-        return "RCTClearCacheModule";
+        return "ClearCache";
     }
 
     @ReactMethod
